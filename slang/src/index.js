162732c2a414e5da2b1943c23d969d0eb9390d41
sanananan3 <127393132+sanananan3@users.noreply.github.com>
2024-04-17 14:41:31 +0900

    first commit

--- slang/src/index.js
+++ slang/src/index.js
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, { Suspense, useRef, useEffect, useState }from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import logo from './flower_logo4.png'
+import { Canvas } from '@react-three/fiber'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { PositionalAudio } from '@react-three/drei'
+
+
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <header className = "App-header">
+      <img src={logo} alt="alt here" style = {{ width: '150px', height: '150px' }} />
+
+    </header>
+    
+    <Canvas>
     <App />
+    </Canvas>
   </React.StrictMode>
+
+
+
+
+
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
