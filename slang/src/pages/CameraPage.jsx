413e696f7cbc416ce37cf649121f94e4bf506c33
jjpark51 <jjpark57@hotmail.com>
2024-05-16 13:51:57 +0900

    update result

--- slang/src/pages/CameraPage.jsx
+++ slang/src/pages/CameraPage.jsx
@@ -159,18 +159,18 @@ const CameraPage = () => {
         </ul>
       </div>
 
-  <div style={{marginLeft: '100px', padding: '1px', height: '100vh', border: '1px solid gray'}}>
+  <div style={{backgroundColor: '#f7f8f9' ,marginLeft: '10px', padding: '1px', height: '100vh', border: '1px solid gray'}}>
     <div style={{ overflowY: 'auto', maxHeight: '100vh' }}>
         <BorderLinearProgress variant="determinate" value={progressValue} style={{boxShadow: 'rgba(0, 0, 0, 0.24) 0px 3px 8px',marginTop: '80px', width: '50%', marginLeft: '25%'}}/>
 
           <p style={{position: 'relative', marginTop: '50px',color:'black', textAlign: 'center' }}>손으로 해당 이미지를 따라해주세요.</p>
           <div id="container" style={{display: 'grid', gridTemplateColumns: '8fr 2fr', gridTemplateRows: 'repeat(3, 1fr)', height: '100%', width: '1300px', columnGap: '100px', marginLeft: '100px'}}>
-            <div style={{gridRow: '1 / span 2',  border: '1px solid gray', borderRadius: '12px', boxShadow: 'rgba(0, 0, 0, 0.24) 0px 3px 8px'}}>
+            <div style={{backgroundColor: 'white' ,gridRow: '1 / span 2',  border: '1px solid gray', borderRadius: '12px', boxShadow: 'rgba(0, 0, 0, 0.24) 0px 3px 8px'}}>
               <img src="http://localhost:5000/video_korean" alt="Video Feed" style={{ width: '100%', height: '60vh' }} />
             </div>
-            <div style={{ boxShadow: 'rgba(0, 0, 0, 0.24) 0px 3px 8px', backgroundImage: `url(${imageUrls[currentImageIndex]})`, backgroundSize: 'contain',backgroundRepeat: 'no-repeat', backgroundPosition: 'center', width: '100%', gridRow: '1', gridColumn: '2',  border: '1px solid gray', borderRadius: '12px'}}>
+            <div style={{backgroundColor: 'white' , boxShadow: 'rgba(0, 0, 0, 0.24) 0px 3px 8px', backgroundImage: `url(${imageUrls[currentImageIndex]})`, backgroundSize: 'contain',backgroundRepeat: 'no-repeat', backgroundPosition: 'center', width: '100%', gridRow: '1', gridColumn: '2',  border: '1px solid gray', borderRadius: '12px'}}>
             </div>
-            <div id="answer_div" style={{boxShadow: 'rgba(0, 0, 0, 0.24) 0px 3px 8px', gridRow: '2', gridColumn: '2', border: '1px solid gray', borderRadius: '12px', width: '100%', height: '90%'}}>
+            <div id="answer_div" style={{backgroundColor: 'white' ,boxShadow: 'rgba(0, 0, 0, 0.24) 0px 3px 8px', gridRow: '2', gridColumn: '2', border: '1px solid gray', borderRadius: '12px', width: '100%', height: '90%'}}>
               <p id="answer_p" style={{color: 'black', marginTop: '200px'}}>결과: <span id="answer_span" style={{color: 'black'}}>    {confirmButtonColor  == '#00cc00' ? currentImageFilename : prediction}</span></p>
             </div>
             <div style={{display: 'flex', justifyContent: 'space-around', columnGap: '500px'}}>
--- slang/src/pages/Result.jsx
+++ slang/src/pages/Result.jsx
@@ -1,122 +1,151 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState } from 'react';
 import '../static/style.css'
 import { useNavigate, useLocation } from 'react-router-dom';
 import LinearProgress, { linearProgressClasses } from '@mui/material/LinearProgress';
 import { styled } from '@mui/material/styles';
 import ApexCharts from 'apexcharts';
-import Dashboard from './Dashboard';
 
-
+const BorderLinearProgress = styled(LinearProgress)(({ theme }) => ({
+  height: 10,
+  borderRadius: 5,
+  // background: 'linear-gradient(to right, #FFD700, #FFC200, #FFB000)',
+    [`&.${linearProgressClasses.colorPrimary}`]: {
+    backgroundColor: theme.palette.grey[theme.palette.mode === 'light' ? 200 : 800],
+  },
+  [`& .${linearProgressClasses.bar}`]: {
+    borderRadius: 5,
+    backgroundColor: theme.palette.mode === 'light' ? 'gold' : '#308fe8',
+  },
+}));
 
 const Result = () => {
   const [activeSpan, setActiveSpan] = useState(null);
   const handleSpanClick = (id) => {
     setActiveSpan(id);
   };
-  const navigate = useNavigate();
-  const [confirmButtonColor, setConfirmButtonColor] = useState('#00cc00');
   const location = useLocation();
-  // const { correctAnswer } = location.state;
-  // console.log("Correct AnsweR: " + correctAnswer)
+  const navigate = useNavigate();
+  const [progressValue, setProgressValue] = useState(50);
 
-  const chartRef = useRef(null);
+  const { correctAnswer } = location.state;
+  const [confirmButtonColor, setConfirmButtonColor] = useState('#00cc00');
+
+
+  // Use the correctAnswer data in your Result component
+
+  // Use the correctAnswer data in your Result component
+  console.log('Correct Answers:', correctAnswer);
+  const [chartOptions, setChartOptions] = useState({
+    chart: {
+      height: 380,
+      type: "radialBar",
+    },
+    series: [(correctAnswer / 14) * 100], // Calculate the accuracy percentage
+    colors: ["#20E647"],
+    plotOptions: {
+      radialBar: {
+        hollow: {
+          margin: 0,
+          size: "70%",
+          background: "#293450",
+        },
+        track: {
+          dropShadow: {
+            enabled: true,
+            top: 2,
+            left: 0,
+            blur: 4,
+            opacity: 0.15,
+          },
+        },
+        dataLabels: {
+          name: {
+            offsetY: -10,
+            color: "#fff",
+            fontSize: "13px",
+          },
+          value: {
+            color: "#fff",
+            fontSize: "30px",
+            show: true,
+            formatter: function (val) {
+              return val.toFixed(1) + "%"; // Format the value as a percentage
+            },
+          },
+        },
+      },
+    },
+    fill: {
+      type: "gradient",
+      gradient: {
+        shade: "dark",
+        type: "vertical",
+        gradientToColors: ["#87D4F9"],
+        stops: [0, 100],
+      },
+    },
+    stroke: {
+      lineCap: "round",
+    },
+    labels: ["정답률"],
+  });
 
   useEffect(() => {
-    const options = {
-      series: [75],
-      chart: {
-        height: 350,
-        type: 'radialBar',
-        toolbar: {
-          show: true,
-        },
-      },
-      plotOptions: {
-        radialBar: {
-          startAngle: -135,
-          endAngle: 225,
-          hollow: {
-            margin: 0,
-            size: '70%',
-            background: '#fff',
-            image: undefined,
-            imageOffsetX: 0,
-            imageOffsetY: 0,
-            position: 'front',
-            dropShadow: {
-              enabled: true,
-              top: 3,
-              left: 0,
-              blur: 4,
-              opacity: 0.24,
-            },
-          },
-          track: {
-            background: '#fff',
-            strokeWidth: '67%',
-            margin: 0,
-            dropShadow: {
-              enabled: true,
-              top: -3,
-              left: 0,
-              blur: 4,
-              opacity: 0.35,
-            },
-          },
-          dataLabels: {
-            show: true,
-            name: {
-              offsetY: -10,
-              show: true,
-              color: '#888',
-              fontSize: '17px',
-            },
-            value: {
-              formatter: function (val) {
-                return parseInt(val);
-              },
-              color: '#111',
-              fontSize: '36px',
-              show: true,
-            },
-          },
-        },
-      },
-      fill: {
-        type: 'gradient',
-        gradient: {
-          shade: 'dark',
-          type: 'horizontal',
-          shadeIntensity: 0.5,
-          gradientToColors: ['#ABE5A1'],
-          inverseColors: true,
-          opacityFrom: 1,
-          opacityTo: 1,
-          stops: [0, 100],
-        },
-      },
-      stroke: {
-        lineCap: 'round',
-      },
-      labels: ['Percent'],
-    };
-
-    const chart = new ApexCharts(chartRef.current, options);
+    const chartElement = document.getElementById('chart');
+    const chart = new ApexCharts(chartElement, chartOptions);
     chart.render();
 
     return () => {
       chart.destroy();
     };
-  }, []);
+  }, [chartOptions]);
+
+
+  const options = {
+    series: [{
+      data: [400, 430, 448, 470, 540, 580, 690,]
+    }],
+    chart: {
+      type: 'bar',
+      height: 350,
+      
+    },
+    plotOptions: {
+      bar: {
+        borderRadius: 4,
+        borderRadiusApplication: 'end',
+        horizontal: false,
+      
+      }
+    },
+    dataLabels: {
+      enabled: false
+    },
+    xaxis: {
+      categories: ['월','화','수','목','금','토','일'
+      ],
+    }
+    ,
+    colors:['#00cc00']
+  };
+
+  useEffect(() => {
+    const chartElement = document.getElementById('week');
+    const chart = new ApexCharts(chartElement, options);
+    chart.render();
+
+    // Clean up function to destroy the chart when the component unmounts
+    return () => {
+      chart.destroy();
+    };
+  }, []); // Empty dependency array to ensure useEffect runs only once after mount
 
   const handleSports = () => {
-    navigate('/camera');
+     navigate('/camera');
 
-   
   }
-
   return (
-    <div style={{display: 'grid', gridTemplateColumns: '10% 90%', height: '100vh', color: 'black'}}>
+    <div style={{display: 'grid', gridTemplateColumns: '12% 88%', height: '100vh', color: 'black'}}>
       <div>
         <ul >
         <span style={{marginTop: '30px', display: 'flex', alignItems: 'center'}}><a class="active" href="#home" style={{color: 'black', textDecoration: 'none'}}>LOGO</a></span>
@@ -170,9 +199,36 @@ const Result = () => {
         </ul>
       </div>
 
-  <div style={{marginLeft: '100px', padding: '1px', height: '100vh', border: '1px solid gray'}}>
+  <div style={{backgroundColor: '#f7f8f9', marginLeft: '10px', padding: '1px', height: '100vh', border: '1px solid gray'}}>
+    <div style={{ overflowY: 'auto', maxHeight: '100vh' }}>
+
+          {/* <p style={{position: 'relative', marginTop: '50px',color:'black', textAlign: 'center' }}>손으로 해당 이미지를 따라해주세요.</p> */}
+          <div id="container" style={{ display: 'grid', gridTemplateColumns: '6fr 4fr', gridTemplateRows: 'repeat(3, 1fr)', height: '90vh', width: '1500px', columnGap: '100px', marginLeft: '100px', marginTop: '3%', rowGap: '40px'}}>
+          <div style={{gridColumn: '1 / span 2', }}>
+          <b style={{marginLeft: '10%', paddingBottom: '20px', color: '#ec9a00', fontWeight: 'bold'}}>GOLD 111</b>
+          <BorderLinearProgress variant="determinate" value={progressValue} style={{ boxShadow: 'rgba(0, 0, 0, 0.24) 0px 3px 8px',marginTop: '80px', width: '80%', margin: 'auto', height:' 40px'}}/>
+
+          </div>
+
+            <div style={{backgroundColor:'white', display: 'flex', flexDirection: 'column', marginTop: '5%', border: '1px solid black', borderRadius: '12px', boxShadow: 'rgba(0, 0, 0, 0.24) 0px 3px 8px',padding: '30px'}}>
+              <h2 style={{margin:'auto'}}>퀴즈 결과</h2>
+              <div id="chart"></div>
+              <button style={{boxShadow: 'rgba(0, 0, 0, 0.24) 0px 3px 8px',width: '100px', backgroundColor: `${confirmButtonColor}`
+              ,cursor: confirmButtonColor == '#00cc00' ? 'pointer' : 'default',
+            }} onClick={handleSports}>확인</button>
+
+            </div>
+              <div style={{backgroundColor:'white', display: 'flex', flexDirection: 'column', marginTop: '5%', border: '1px solid black', borderRadius: '12px', boxShadow: 'rgba(0, 0, 0, 0.24) 0px 3px 8px',padding: '30px'}}>
+                  <h2 style={{margin:'auto'}}>이번 주 학습</h2>
+                  <div id="week"></div>
+
+            </div>
+          </div>
+           
+        </div>
+
   </div>
-    <h1>TEST</h1>
+    
     </div>
   );
   
